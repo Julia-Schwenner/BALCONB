3970c607200a98e074bdd5a02665f0c23bcbf1e1
Katharina Kramer <katharinavk@icloud.com>
2020-05-26 16:58:36 +0200

    Added dogs index overview page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+before_action :set_dog, only: [:show]
+skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @dogs = Dog.all
+  end
+
+  # def show
+  # end
+
+  private
+
+  # def set_dog
+  #   @dog = Dog.find(params[:id])
+  # end
+
+  # def cocktails_params
+  #   params.require(:dog).permit(:name, :address, )
+  #   # later :photo
+  # end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  # validates :name, uniqueness: { scope: :user_id }
+  validates :price, presence: true
 end
