2cab090ec4a80099ae3bd6e8cb7e09e42eeb3f8e
Mealedey In <inmeallie@gmail.com>
2020-05-27 14:27:04 +0200

    Meetings-MVC

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,17 @@
 class MeetingsController < ApplicationController
+  def index
+    @user_meetings = Meetings.all #where @user = current_user
+  end
+
+  def create
+    @user = current_user
+
+    @meeting = Meeting.new(meeting_params)
+    @dog.user = @user
+    if @dog.save
+      redirect_to dogs_path
+    else
+      render :new
+    end
+  end
 end
