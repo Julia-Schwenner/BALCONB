d33404d85027dfb6cd111340aa96fd0ecc0d9d9e
Julia Schwenner <julia.schwenner@gmail.com>
2020-05-27 11:09:12 +0200

    createdoglisting

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,33 @@
 class DogsController < ApplicationController
-end
+
+      
+        #def show
+        #end
+      
+        def new
+          @dog = Dog.new
+        end
+      
+        def create
+            @user = current_user
+          @dog = Dog.new(dog_params)
+          @dog.user = @user
+          if @dog.save
+            redirect_to dogs_path
+          else
+            render :new
+          end
+        end
+      
+        def edit
+        end
+
+        private
+      
+        #strong params
+        def dog_params
+          params.require(:dog).permit(:name, :address, :description, :price)
+        end
+
+end 
+
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
 class Dog < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
